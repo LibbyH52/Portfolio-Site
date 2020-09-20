c4727bcc305fe9e34496c53d559c5e16feec190d
Libby Henry <libbyh52@gmail.com>
2020-09-20 17:09:23 +0100

    Navbar code with font-awesome icons added to HTML, styles and colours for same. JS code for toggling navbar on mobile screens

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,17 @@
-const navLink = document.querySelector('.navbar-collapse');
-const links = navLink.querySelectorAll('li a');
-const  close = document.querySelector('.navbar-collapse');
+/*jslint browser */
+/*ES6*/
+
+const navigate = document.querySelector('ul');
+const bars = document.querySelector('#icon-bars');
+const links = document.querySelectorAll('.nav-link');
+
+bars.addEventListener('click', (e) => {
+    'use strict';
+    navigate.classList.toggle('hidden');
+});
 
 links.forEach(link => {
-    link.addEventListener("click", (e) => {
-        close.classList.toggle("show")
-    });
-});
+    link.addEventListener('click', (e) => {
+        navigate.classList.add('hidden');
+    })
+})
