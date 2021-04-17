1d044b2411adf32516c0360ad2c48eca1046ba31
Libby Henry <libbyh52@gmail.com>
2021-04-17 18:58:48 +0100

    Function for toggling the display of the nav element when the user clicks on the nav button or one of the links

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,26 @@
 /*jslint browser */
 /*ES6*/
-const navigate = document.querySelector('.nav-menu');
+const navigate = document.querySelector('.navigation');
 const bars = document.querySelector('.btn-icon-bars');
 const links = document.querySelectorAll('.nav-link');
 
+
+const toggleNavigation = () => {
+    if(navigate.style.display === "none") {
+        navigate.style.display = "flex";
+    } else {
+        navigate.style.display="none";
+    }
+}
+
 bars.addEventListener('click', (e) => {
     'use strict';
-    navigate.classList.toggle('hidden');
+    // navigate.classList.toggle('hidden'); 
+    toggleNavigation();
 });
 
 links.forEach(link => {
     link.addEventListener('click', (e) => {
-        navigate.classList.add('hidden');
+       toggleNavigation();
     })
 })
